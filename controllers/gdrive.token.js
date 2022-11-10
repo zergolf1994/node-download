feaa157fbe5a7d2b5cfd0a2aa509c466942c8e66
zergolf1994 <zergolf1994@gmail.com>
2022-11-10 20:10:25 +0700

    Add token

--- controllers/gdrive.token.js
+++ controllers/gdrive.token.js
@@ -3,22 +3,26 @@
 const shell = require("shelljs");
 
 module.exports = async (req, res) => {
-    
   const { token } = req.query;
 
   try {
-    if (!token) return res.json({ status: false });
+    if (!token) {
+      console.warn("Warning", "no token");
+      return res.json({ status: false });
+    }
 
     let data_out = await driveInfo();
     let error404 = /Failed to get file/i;
 
     if (error404.test(data_out)) {
+      console.error("Error", "cannot connect");
       return res.json({ status: false, msg: "error" });
     } else {
+      console.log("Update", "cannot connect");
       return res.json({ status: true, msg: "update token" });
     }
   } catch (error) {
-    console.log(error);
+    console.error(error);
     return res.json({ status: false, msg: error.name });
   }
 
@@ -26,7 +30,14 @@ module.exports = async (req, res) => {
     //console.log("driveInfo")
     return new Promise(function (resolve, reject) {
       shell.exec(
-        `printf "${token}" | gdrive info ${gid}`,
+        `cd && rm -rf .gdrive/*`,
+        { async: true, silent: true },
+        function (code, stdout, stderr) {
+          console.log("Remove token");
+        }
+      );
+      shell.exec(
+        `printf "${token}" | gdrive info 11HMaFfheJprmnK8QEJquuJIT913DGJJ4`,
         { async: true, silent: true },
         function (code, stdout, stderr) {
           //console.log(stdout)
