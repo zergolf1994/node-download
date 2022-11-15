c95421f20ae48ce045a56816117491fb053a72a9
zergolf1994 <zergolf1994@gmail.com>
2022-11-15 12:52:43 +0700

    chacng

--- controllers/server.update.js
+++ controllers/server.update.js
@@ -3,15 +3,21 @@
 const path = require("path");
 const fs = require("fs");
 const Servers = require("../modules/Mysql/Servers");
+const shell = require("shelljs");
+const { SettingValue, timeSleep } = require("../modules/Function");
 
 module.exports = async (req, res) => {
-  const { sv_ip, app_api } = req.query;
+  const { sv_ip } = req.query;
   try {
     if (!sv_ip) return res.json({ status: false });
 
+    let { domain_api } = await SettingValue(true);
+
+    if(!domain_api) return res.json({ status: false, msg: `not_domain_api` });
+
     let where = {},
       data = {};
-    where.active = {[Op.ne]:2};
+    where.active = { [Op.ne]: 2 };
     where.sv_ip = sv_ip;
     where.type = "download";
 
@@ -30,7 +36,7 @@ module.exports = async (req, res) => {
       );
 
       shell.exec(
-        `sudo cd && rm -rf update.sh wget -q http://${app_api}/install/download/update.sh && chmod +x update.sh && sudo bash update.sh`,
+        `sudo cd && rm -rf update.sh wget -q http://${domain_api}/install/download/update.sh && chmod +x update.sh && sudo bash update.sh`,
         { async: false, silent: false },
         function (data) {}
       );
